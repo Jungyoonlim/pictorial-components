116fb242512068475b74450ffd5ac0e19a43c531
Joanne <jungyoon.lim99@gmail.com>
2024-09-30 13:30:13 +0900

    fotn size responsive desing

--- b2b-saas-visualizer/src/styles.js
+++ b2b-saas-visualizer/src/styles.js
@@ -40,23 +40,50 @@ export const OrganizationsGrid = styled.div`
 
     @media (max-width: 480px){
         grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
-        
-    
+        gap: 16px; 
     }
 `
 
-
-export const HeaderTitle = styled.div`
+export const HeaderTitle = styled.h1`
     font-size: var(--header-font-size, 80px); 
-    position: absolute;
-    width: 777px;
-    height: 196px; 
-    left: 46px; 
-    top: 369px; 
-    font-family: 'Instrument Sans', sans-serif;
-    font-weight: 400;
-    font-size: 80px;
-    line-height: 98px;
+    line-height: 1.2; 
+    position: relative;
+    z-index: 1; 
     color: #FFFFFF; 
+    text-align: center; 
+
+    /* Proportional width, height, and positioning */
+    width: calc(777 / 1440 * 100vw);
+    height: calc(196 / 900 * 100vh);
+    left: calc(46 / 1440 * 100vw);
+    top: calc(369 / 900 * 100vh);
+
+    font-family: 'Instrument Sans', sans-serif;
+
+    /* Adjust the font size for diff. screen sizes! */
+    @media (max-width: 1200px) {
+        font-size: 60px; 
+        left: calc(46 / 1200 * 100vw);
+        right: calc(369 / 800 * 100vh); 
+    }
+
+    @media (max-width: 768px) {
+        font-size: 40px;
+        left: calc(46 / 768 * 100vw);
+        right: calc(369 / 600 * 100vh);
+    }
+
+    @media (max-width: 480px) {
+        font-size: 30px;
+        left: calc(46 / 480 * 100vw);
+        top: calc(369 / 400 * 100vh);
+    }
 `;
 
+export const OrganizationCard = styled(motion.div)`
+    background-color: ${({ bgColor }) => bgColor || "#B5651D"}
+    border-radius: 20px;
+    padding: 20
+
+
+`
